03f7053de0a6cd47e4903c872f8678c78028ce1c
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-07 21:14:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Plindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word to check if its palindrome: ");
+            string word = Console.ReadLine().ToLower().Trim();
+            bool isPalindrome = true;
+            int length = word.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (word[i] != word[length - 1 - i])
+                {
+                    isPalindrome = false; 
+                    break; 
+                }
+
+
+            }
+            if (isPalindrome)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
+
